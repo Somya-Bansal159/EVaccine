67c41bf251e9fe4f52654d533f2b9845af4c31fe
Somya Bansal <somya.bansal.cse20@itbhu.ac.in>
2022-12-15 22:20:04 +0530

    Add files via upload

---
+++ sql files to run to prepopulate the database/filling_centres.sql
@@ -0,0 +1,21 @@
+insert into registration_centers
+values 
+	(100001, 'A', 'A1', 'A1_centre1', 100, 100, 100),
+    (default, 'A', 'A1', 'A1_centre2', 100, 100, default),
+    (default, 'A', 'A2', 'A2_centre1', default, 100, 100),
+    (default, 'A', 'A2', 'A2_centre2', 100, 100, 100),
+    (default, 'A', 'A3', 'A3_centre1', 100, 100, default),
+    (default, 'A', 'A3', 'A3_centre2', 100, default, 100),
+    (default, 'B', 'B1', 'B1_centre1', default, 100, 100),
+    (default, 'B', 'B1', 'B1_centre2', 100, 100, 100),
+    (default, 'B', 'B2', 'B2_centre1', 100, default, 100),
+    (default, 'B', 'B2', 'B2_centre2', 100, 100, 100),
+    (default, 'B', 'B3', 'B3_centre1', 100, 100, default),
+    (default, 'B', 'B3', 'B3_centre2', default, 100, 100),
+    (default, 'C', 'C1', 'C1_centre1', 100, default, default),
+    (default, 'C', 'C1', 'C1_centre2', default, 100, 100),
+    (default, 'C', 'C2', 'C2_centre1', 100, 100, 100),
+    (default, 'C', 'C2', 'C2_centre2', 100, 100, default),
+    (default, 'C', 'C3', 'C3_centre1', default, 100, 100),
+    (default, 'C', 'C3', 'C3_centre2', 100, default, 100)
+
---
+++ sql files to run to prepopulate the database/filling_statuses.sql
@@ -0,0 +1,5 @@
+insert into registration_statuses
+values
+	(1, 'not booked', 'not booked'),
+    (2, 'booked', 'not booked'),
+    (3, 'booked', 'booked')
